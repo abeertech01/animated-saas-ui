558ee7137fb6144206127125f9e4b6f1eb996c44
abeertech01 <abeer.technology@gmail.com>
2026-02-07 15:14:22 +0600

    cal to action animation

--- sections/CallToAction.tsx
+++ sections/CallToAction.tsx
@@ -1,15 +1,43 @@
+"use client"
+
+import { AnimationPlaybackControls, motion, useAnimate } from "motion/react"
+import { useEffect, useRef, useState } from "react"
+
 export default function CallToAction() {
+  const [isHovered, setIsHovered] = useState(false)
+  const animation = useRef<AnimationPlaybackControls>(null)
+  const [scope, animate] = useAnimate()
+
+  useEffect(() => {
+    animation.current = animate(
+      scope.current,
+      { x: "-50%" },
+      { duration: 30, ease: "linear", repeat: Infinity },
+    )
+  }, [])
+
+  useEffect(() => {
+    if (animation.current) {
+      if (isHovered) animation.current.speed = 0.5
+      else animation.current.speed = 1
+    }
+  }, [isHovered])
   return (
     <section className="py-24">
       <div className="flex p-4 overflow-x-clip">
-        <div className="flex flex-none gap-16 font-medium text-7xl md:text-8xl">
+        <motion.div
+          ref={scope}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          className="group flex flex-none gap-16 pr-16 font-medium text-7xl md:text-8xl cursor-pointer"
+        >
           {Array.from({ length: 10 }).map((_, i) => (
             <div key={i} className="flex items-center gap-16">
               <span className="text-lime-400 text-7xl">&#10038;</span>
-              <span>Try it for free</span>
+              <span className="group-hover:text-lime-400">Try it for free</span>
             </div>
           ))}
-        </div>
+        </motion.div>
       </div>
     </section>
   )
